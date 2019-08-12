6399c47d5be2a53367a4df22e86ef1d54654badd
prithvi venu <prithviv30@gmail.com>
2019-08-13 04:43:01 +0530

    Add project files.

---
+++ LearnSqlite/MainPage.xaml.cs
@@ -0,0 +1,67 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Storage;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace LearnSqlite
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public class Stock
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Symbol { get; set; }
+    }
+
+    public class Valuation
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [Indexed]
+        public int StockId { get; set; }
+        public DateTime Time { get; set; }
+        public decimal Price { get; set; }
+    }
+    public class Valuationn
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        [Indexed]
+        public int StockId { get; set; }
+        public DateTime Time { get; set; }
+        public decimal Price { get; set; }
+    }
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            string path = Path.Combine(Windows.Storage.ApplicationData.
+  Current.LocalFolder.Path, "learn.db");
+            var db = new SQLiteConnection(path);
+
+            db.CreateTable<Stock>();
+            db.CreateTable<Valuation>();
+            db.CreateTable<Valuationn>();
+
+
+
+        }
+    }
+}
